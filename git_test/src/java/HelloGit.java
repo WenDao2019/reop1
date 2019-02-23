de2587630ff76fedf6002bcdc3f5e10940d4dfb5
WenDao <852075824@qq.com>
2019-02-23 19:35:29 +0800

    添加git_test项目

---
+++ git_test/src/java/HelloGit.java
@@ -0,0 +1,11 @@
+package java;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        //123
+    }
+}
